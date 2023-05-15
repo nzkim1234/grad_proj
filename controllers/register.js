25a9e173ca2031d3ca67e9c88848cc8488ac10f5
nzkim1234 <nzkim1234@naver.com>
2023-05-15 21:02:13 +0900

    make user_db when register

--- controllers/register.js
+++ controllers/register.js
@@ -23,8 +23,24 @@ module.exports = {
                                 console.log(err) ;
                                 return res.status(400).end();
                             }
-                            else return res.status(200).end();
+                            else {
+                                db.query('SELECT seq FROM users WHERE id=?', userId, (err, row) => {
+                                    const seq = row[0].seq;
+                                    const seq_db = seq + '_data'
+                                    db.query(`CREATE TABLE ${seq_db} (prod_name VARCHAR(45) NOT NULL, intake_per_day INT(10) UNSIGNED ZEROFILL DEFAULT NULL, vitaminA INT(10) UNSIGNED ZEROFILL DEFAULT NULL, vitaminD INT(10) UNSIGNED ZEROFILL DEFAULT NULL, vitaminE INT(10) UNSIGNED ZEROFILL DEFAULT NULL, vitaminK INT(10) UNSIGNED ZEROFILL DEFAULT NULL, vitaminB1 INT(10) UNSIGNED ZEROFILL DEFAULT NULL, vitaminB2 INT(10) UNSIGNED ZEROFILL DEFAULT NULL, vitaminB6 INT(10) UNSIGNED ZEROFILL DEFAULT NULL, vitaminB12 INT(10) UNSIGNED ZEROFILL DEFAULT NULL, vitaminC INT(10) UNSIGNED ZEROFILL DEFAULT NULL, nicotinic_acid INT(10) UNSIGNED ZEROFILL DEFAULT NULL, pantothenic INT(10) UNSIGNED ZEROFILL DEFAULT NULL, folic_acid INT(10) UNSIGNED ZEROFILL DEFAULT NULL, biotin INT(10) UNSIGNED ZEROFILL DEFAULT NULL, calcium INT(10) UNSIGNED ZEROFILL DEFAULT NULL, magnesium INT(10) UNSIGNED ZEROFILL DEFAULT NULL,iron INT(10) UNSIGNED ZEROFILL DEFAULT NULL, copper INT(10) UNSIGNED ZEROFILL DEFAULT NULL, selenium INT(10) UNSIGNED ZEROFILL DEFAULT NULL, iodine INT(10) UNSIGNED ZEROFILL DEFAULT NULL, manganese INT(10) UNSIGNED ZEROFILL DEFAULT NULL, molybdenum INT(10) UNSIGNED ZEROFILL DEFAULT NULL, chrome INT(10) UNSIGNED ZEROFILL DEFAULT NULL, UNIQUE KEY prod_name_UNIQUE (prod_name))`, (err, row) =>{
+                                            if(err) {
+                                                console.log(err);
+                                                return res.status(400).end();
+                                            }
+                                            else{
+                                                console.log("db_made")
+                                                return res.status(200).end();
+                                            }
+                                    })
+                                })
+                            }
                         })
+                                
                     }
                 })
             }
