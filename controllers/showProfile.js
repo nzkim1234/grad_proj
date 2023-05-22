53a393853847dc600bd2ac392247f61243521715
nzkim1234 <nzkim1234@naver.com>
2023-05-23 03:29:25 +0900

    showprofile change

--- controllers/showProfile.js
+++ controllers/showProfile.js
@@ -4,12 +4,10 @@ const fs = require('fs');
 module.exports = {
     getProfileImage : async (req, res, next) => {
         const profile_img = req.query.profile_img;
-        console.log(req);
-        console.log(req.query)
         console.log(`${path.resolve(__dirname, "../public/images/")}/${profile_img}.png`);
-        const fileExist = (fs.existsSync(`${path.resolve(__dirname, "../public/images/")}/${profile_img}`));
+        const fileExist = (fs.existsSync(`${path.resolve(__dirname, "../public/images/")}/${profile_img}_image.png`));
         if (fileExist){
-            const image = fs.readFileSync(`${path.resolve(__dirname, "../public/images/")}/${profile_img}`);
+            const image = fs.readFileSync(`${path.resolve(__dirname, "../public/images/")}/${profile_img}_image.png`);
             return res.status(200).send(image);
         }
         else {
