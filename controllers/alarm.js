e5623ad44a7274d7940295c037e0719625e9d570
nzkim1234 <nzkim1234@naver.com>
2023-05-30 01:45:32 +0900

    test

--- controllers/alarm.js
+++ controllers/alarm.js
@@ -4,7 +4,7 @@ module.exports = {
     postAddAlarm: async(req, res, next) => {
         const [seq, time, days, alarm_name, box, vitamin] = [req.body.seq, req.body.time, req.body.days, req.body.alarmname, req.body.box, req.body.vitamin];
         console.log(days);
-        db.query(`INSERT INTO ${seq}_alarm (alarm_name, alarm_time, days, box, vitamin) VALUES (${alarm_name}, ${time}, ${days}, ${box}, ${vitamin})`, (err, row) => {
+        db.query(`INSERT INTO ${seq}_alarm (alarm_name, alarm_time, days, box, vitamin) VALUES (${alarm_name}, ${time}, '${days}', ${box}, ${vitamin})`, (err, row) => {
             if(err) {
                 console.log(err)
                 return res.send(400).send(err).end();
