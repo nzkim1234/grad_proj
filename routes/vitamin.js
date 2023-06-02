280cc62721cd2a3d265807be4a279f127efdc3fa
nzkim1234 <nzkim1234@naver.com>
2023-06-02 17:32:12 +0900

    fix search

--- routes/vitamin.js
+++ routes/vitamin.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/vitamin');   
 
-router.get('/search',controller.postSearchVitamin);
+router.get('/search',controller.getSearchVitamin);
 router.post('/add', controller.postAddVitamin);
 router.get('/get', controller.getVitamin); 
 router.post('/remove', controller.postRemoveVitamin); 
